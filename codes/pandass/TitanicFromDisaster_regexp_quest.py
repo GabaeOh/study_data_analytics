32c66858c70374976510c59674d2910bf6072408
GabaeOh <rkqo4577@gmail.com>
2023-08-22 19:32:36 +0900

    끄읏

--- codes/pandass/TitanicFromDisaster_regexp_quest.py
+++ codes/pandass/TitanicFromDisaster_regexp_quest.py
@@ -20,7 +20,20 @@ df_Name_middlename = df_Name.str.extract(pattern_middlename)
 
 #컬럼추가 
 df_Name['firstname']= df_Name.str.extract(pattern_firstname)
-#print(df_Name)
+#print(df_Name['firstname'])
 
 df_Name['middlename']= df_Name.str.extract(pattern_middlename)
-print(df_Name)
+#print(df_Name['middlename'])
+
+
+# apply 함수를 사용하여 중간 이름 추출
+def extract_middle_name(name):
+    pattern = r'([A-Za-z]+)\.'
+    result = re.findall(pattern, name)
+    if result:
+        return result[0]
+    else:
+        return None
+
+df_Name['MiddleName(apply적용)'] = df_Name.apply(extract_middle_name)
+print(df_Name['MiddleName(apply적용)'])
